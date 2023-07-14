586b48f06d159f135e8f03c397c2ba873fb1db5b
Athif Binu <binuathif@gmail.com>
2023-07-14 11:43:55 +0530

    change btn clr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,39 @@
-import React, { useRef } from 'react'
-import logo from "../assets/images/logo.9abef1d7.svg"
-import {BsGlobe} from "react-icons/bs"
-import {AiOutlineMenu} from "react-icons/ai"
-import {FaTimes} from "react-icons/fa"
-import "../styles/header.css"
+import React, { useRef } from "react";
+import logo from "../assets/images/logo.9abef1d7.svg";
+import { BsGlobe } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa";
+import "../styles/header.css";
 
 const Header = () => {
+  const navRef = useRef();
 
-
-
-  const navRef =useRef();
-
-
-  const showNavbar= ()=>{
-    navRef.current.classList.toggle(
-      "responsive_nav"
-    )
-  }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
     <header className="header">
       <div className="logo">
-         <img src={logo} alt="" />
-         <h2>OmniBridge</h2>
+        <img src={logo} alt="" />
+        <h2>OmniBridge</h2>
       </div>
 
       <div className="btn" ref={navRef}>
         <button>Connect Wallet</button>
         <div className="globe-drop">
-           <BsGlobe size={18}  />
-            <p>ETH {'=>'}GC</p>
-            
+          <BsGlobe size={18} />
+          <p>ETH {"=>"}GC</p>
         </div>
-        <FaTimes size={20}  onClick={showNavbar} className='nav-btn nav-close-btn'/>
+        <FaTimes
+          size={20}
+          onClick={showNavbar}
+          className="nav-btn nav-close-btn"
+        />
       </div>
 
-    
-  
-        <AiOutlineMenu size={20} onClick={showNavbar} className='nav-btn'/>
-      
+      <AiOutlineMenu size={20} onClick={showNavbar} className="nav-btn" />
+    </header>
+  );
+};
 
-    
-  
-  </header>
-  )
-}
-
-export default Header
+export default Header;
